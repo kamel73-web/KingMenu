770394272e2d85a33bf4b82a50626d4ebf7966b3
kamel73-web <62759979+kamel73-web@users.noreply.github.com>
2026-01-31 23:29:50 +0100

    Add path alias for src directory in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   base: "/KingMenu/",
+
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   define: {
     "import.meta.env.VITE_SUPABASE_URL": JSON.stringify("https://vehqvqlbtotljstixklz.supabase.co"),
     "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlaHF2cWxidG90bGpzdGl4a2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1OTAxOTEsImV4cCI6MjA2NjE2NjE5MX0.TnSx9bjz8wqo3pBPHaW11YtFcNYHg7Fckuo8z32rG4w"),
