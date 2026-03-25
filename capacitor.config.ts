6978a7814385e9d1b635b47db6bd972dccc39f91
kamel73-web <62759979+kamel73-web@users.noreply.github.com>
2026-03-25 12:14:37 +0000

    fix: capacitor config simplifié + keystore régénéré v1.0.24

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,16 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.kingmenu.app',
   appName: 'King Menu',
   webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    androidScheme: 'https',
-    cleartext: false,
-  },
-  android: {
-    allowMixedContent: false,
-    captureInput: true,
-    webContentsDebuggingEnabled: false,
-  }
 };
 
 export default config;
