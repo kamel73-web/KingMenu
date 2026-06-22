c9384337afdcf3b87619a4010529cbfda53ae191
kamel73-web <62759979+kamel73-web@users.noreply.github.com>
2026-06-22 15:57:00 +0100

    Add WebPlugin class and registerPlugin function

--- src/stubs/capacitor-core.ts
+++ src/stubs/capacitor-core.ts
@@ -1,3 +1,22 @@
+export class WebPlugin {
+  constructor() {}
+
+  protected async handleRequest(
+    options: any
+  ): Promise<any> {
+    return options;
+  }
+}
+
+
+export function registerPlugin(
+  name: string,
+  implementations?: any
+) {
+  return implementations?.web ?? {};
+}
+
+
 export const Capacitor = {
   isNativePlatform: () => false,
   getPlatform: () => 'web',
